59dd64d3e1ff0ff7f6f9c1f61bad5a440f413ba2
MupLever <8willox8@gmail.com>
2024-02-10 23:02:00 +0300

    собираются координаты улиц, а не домов

--- configs.py
+++ configs.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+URL = os.getenv("URL")
+
 GECKODRIVER_PATH = os.getenv("GECKODRIVER_PATH")
 ADDRESSES_FILE = os.getenv("ADDRESSES_FILE")
 NEW_ADDRESSES_FILE = os.getenv("NEW_ADDRESSES_FILE")
--- main.py
+++ main.py
@@ -15,8 +15,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, WebDriverException
 
-from configs import GECKODRIVER_PATH, ADDRESSES_FILE, NEW_ADDRESSES_FILE
-from utils import wait_until_text_on_page, get_parse_result
+from configs import GECKODRIVER_PATH, ADDRESSES_FILE, NEW_ADDRESSES_FILE, URL
+from utils import get_parse_result
 
 SECOND_TO_WAIT_INPUT = 20
 SECOND_TO_WAIT_BUTTON = 20
@@ -32,8 +32,7 @@ logger = logging.getLogger("FireFox")
 
 def parse_coordinates(
     address: str, driver: Firefox
-) -> Tuple[str, Optional[str], Optional[str], Optional[str]]:
-    msg = ""
+) -> Tuple[str, Optional[str], Optional[str]]:
     input_xpath = (
         "//span[@class='input__context']/input[@placeholder='Поиск мест и адресов']"
     )
@@ -43,7 +42,7 @@ def parse_coordinates(
         )
     except TimeoutException:
         msg = "Timeout waiting for input to appear"
-        return msg, None, None, None
+        return msg, None, None
 
     field = driver.find_element(by="xpath", value=input_xpath)
     field.clear()
@@ -58,31 +57,17 @@ def parse_coordinates(
         )
     except TimeoutException:
         msg = f"{address} is not a valid address"
-        return msg, None, None, None
+        return msg, None, None
 
     button = driver.find_element(by="xpath", value=button_xpath)
     button.click()
 
-    timeout = random() * 5
+    timeout = random() * 2.5
     time.sleep(timeout)
-
-    wait_until_text_on_page(driver=driver, texts=["координаты:"], timeout=5)
 
     coordinates_pattern = re.compile(r"(?P<lat>.+), (?P<lng>.+)")
 
     data = Selector(text=driver.page_source)
-    try:
-        house = (
-            data.xpath("//h1[@class='card-title-view__title']/text()")
-            .get()
-            .split(", ")[1]
-        )
-    except AttributeError:
-        msg = f"No house {address}"
-        return msg, None, None, None
-    except IndexError:
-        msg = f"No house {address}"
-        return msg, None, None, None
 
     coordinates_item = data.xpath(
         "//div[@class='clipboard__content']/div/div[@class='toponym-card-title-view__coords-badge']/text()"
@@ -93,7 +78,7 @@ def parse_coordinates(
     lng = coordinates_info.get("lng", "")
     lat = coordinates_info.get("lat", "")
 
-    return msg, house, lng, lat
+    return "", lng, lat
 
 
 def main(url: str, driver: Firefox) -> None:
@@ -103,32 +88,36 @@ def main(url: str, driver: Firefox) -> None:
         logger.error(f"Can't get {url}: {exc}")
         return
 
-    time.sleep(20)
+    time.sleep(10)
 
     with open(ADDRESSES_FILE, "r", encoding="utf-8") as readable_file:
         with open(NEW_ADDRESSES_FILE, "a+", encoding="utf-8") as writable_file:
             for line in readable_file:
                 address_dict = json.loads(line)
-                for house in address_dict["houses"]:
-                    address = f"Москва, {address_dict['street']}, {house}"
-                    error_message, corrected_house, lng, lat = parse_coordinates(
-                        address, driver
+                district = (
+                    address_dict["district"]
+                    if any(
+                        settlement in address_dict["district"]
+                        for settlement in ["деревня", "посёлок"]
                     )
-                    if error_message:
-                        logger.error(error_message)
-                        continue
+                    else "Москва"
+                )
+                address = f"{district}, {address_dict['street']}"
+                error_message, lng, lat = parse_coordinates(address, driver)
+                if error_message:
+                    logger.error(error_message)
+                    continue
 
-                    new_address_dict = {
-                        "district": address_dict["district"],
-                        "street": address_dict["street"],
-                        "house": corrected_house,
-                        "lng": lng,
-                        "lat": lat,
-                    }
-                    logger.info(f"SUCCESS: {new_address_dict}")
-                    writable_file.write(
-                        json.dumps(new_address_dict, ensure_ascii=False) + "\n"
-                    )
+                new_address_dict = {
+                    "district": address_dict["district"],
+                    "street": address_dict["street"],
+                    "lng": lng,
+                    "lat": lat,
+                }
+                logger.info(f"SUCCESS: {new_address_dict}")
+                writable_file.write(
+                    json.dumps(new_address_dict, ensure_ascii=False) + "\n"
+                )
 
 
 if __name__ == "__main__":
@@ -136,6 +125,6 @@ if __name__ == "__main__":
     driver = webdriver.Firefox(service=service)
     driver.maximize_window()
     main(
-        url="https://yandex.ru/maps/213/moscow/",
+        url=URL,
         driver=driver,
     )
